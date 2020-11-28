d43561103a018c6767eefcb0148ec17199a56ee4
Ajay Sahani <ajay.sahani@connectwise.com>
2020-11-28 21:03:32 +0530

    added log info for request handler

--- src/service/health.go
+++ src/service/health.go
@@ -2,11 +2,13 @@ package service
 
 import (
 	"net/http"
+	"practice/gomicro/src/app"
 	"practice/gomicro/src/lib"
 	"practice/gomicro/src/model"
 )
 
 // handlerAppHealth used for application health handler
 var handlerAppHealth = func(w http.ResponseWriter, r *http.Request) {
+	app.Service.LoggerService.Info("health api")
 	lib.RenderJSON(w, r, model.AppHealth())
 }
--- src/service/version.go
+++ src/service/version.go
@@ -2,11 +2,13 @@ package service
 
 import (
 	"net/http"
+	"practice/gomicro/src/app"
 	"practice/gomicro/src/lib"
 	"practice/gomicro/src/model"
 )
 
 // handlerAppVersion used for application version handler
 var handlerAppVersion = func(w http.ResponseWriter, r *http.Request) {
+	app.Service.LoggerService.Info("version api")
 	lib.RenderJSON(w, r, model.AppVersion())
 }
